ee5dc813e5b3a8d6b7cc549a828d88077ff10aaf
Richard Miller <richardmmill@gmail.com>
2021-08-12 15:29:50 -0400

    static express method

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-console.log(__dirname);
+app.use(express.static('client'));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../client/index.html'))
 });
 
 const port = process.env.PORT || 5050;
